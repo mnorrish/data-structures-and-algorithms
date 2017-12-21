72b003e726a946989cab675140240457bbbdcc0b
Matthew Norrish <matt.norrish@clavisinsight.com>
2017-12-22 08:54:43 +1300

    improve mergeSort by detecting remainder insertion

--- source/mergeSort/index.spec.ts
+++ source/mergeSort/index.spec.ts
@@ -1,10 +1,23 @@
-import mergeSort, { merge } from '.';
+import mergeSort, { split, merge } from '.';
+
+describe('split', () => {
+  it('splits given array in half', () => {
+    expect(split([])).toEqual([[], []])
+    expect(split([1])).toEqual([[], [1]]);
+    expect(split([1, 2])).toEqual([[1], [2]]);
+    expect(split([1, 2, 3])).toEqual([[1], [2, 3]]);
+    expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
+    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2], [3, 4, 5]]);
+  });
+});
 
 describe('merge', () => {
   it('merges given sorted arrays correctly', () => {
+    expect(merge([2], [1])).toEqual([1, 2]);
     expect(merge([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
     expect(merge([3, 4], [1, 2])).toEqual([1, 2, 3, 4]);
     expect(merge([1, 4], [2, 3])).toEqual([1, 2, 3, 4]);
+    expect(merge([2, 3, 5, 6, 7], [1, 4, 8])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 });
 
--- source/mergeSort/index.ts
+++ source/mergeSort/index.ts
@@ -2,14 +2,31 @@ type nums = number[];
 
 export function merge(left: nums, right: nums): nums {
   const result = [];
+  const leftLast = left[left.length - 1];
+  const rightLast = right[right.length - 1];
   let leftIndex = 0;
   let rightIndex = 0;
 
   while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      result.push(left[leftIndex++]);
+    const leftValue = left[leftIndex];
+    const rightValue = right[rightIndex];
+
+    if (leftLast <= rightValue) {
+      // if remainder of left inserts before next right
+      result.push(...left.slice(leftIndex));
+      leftIndex = left.length;
+    } else if (rightLast < leftValue) {
+      // if remainder or right inserts before next left
+      result.push(...right.slice(rightIndex));
+      rightIndex = right.length;
+    } else if (leftValue < rightValue) {
+      // if next left inserts before next right
+      result.push(leftValue);
+      leftIndex++;
     } else {
-      result.push(right[rightIndex++]);
+      // if next right inserts before next left
+      result.push(rightValue);
+      rightIndex++;
     }
   }
 
@@ -20,13 +37,19 @@ export function merge(left: nums, right: nums): nums {
   ];
 }
 
+export function split<T>(array: Array<T>): Array<T>[] {
+  const middleIndex = Math.floor(array.length / 2);
+
+  return [
+    array.slice(0, middleIndex),
+    array.slice(middleIndex),
+  ];
+}
+
 export default function mergeSort(array: nums): nums {
   if (array.length <= 1) return array;
 
-  const middleIndex = Math.floor(array.length / 2);
-
-  const left = array.slice(0, middleIndex);
-  const right = array.slice(middleIndex);
+  const [left, right] = split(array);
   
   return merge(mergeSort(left), mergeSort(right));
 }
